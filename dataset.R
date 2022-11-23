51fd845f233b8bf0a5a1bc49d9267a2086c08d6b
federicobrogi <81154729+federicobrogi@users.noreply.github.com>
2022-11-23 22:45:39 +0100

    Delete dataset.R

--- dataset.R
+++
@@ -1,9 +0,0 @@
-######## REGRESSION ADJUSTMENT Read data #######
-load("JTRAIN2.Rda")
-JTRAIN2<-as.data.frame(JTRAIN2)
-vars <- names(JTRAIN2)
-
-### subset 
-JTRAIN2_W1 <- JTRAIN2 %>% filter(train==1) 
-JTRAIN2_W0 <- JTRAIN2 %>% filter(train==0)
-################################################
