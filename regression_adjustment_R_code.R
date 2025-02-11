de8bd503b0d98e0c5aca641dab740b5b37b23869
federicobrogi <81154729+federicobrogi@users.noreply.github.com>
2025-02-11 15:42:48 +0100

    Update regression_adjustment_R_code.R

--- regression_adjustment_R_code.R
+++ regression_adjustment_R_code.R
@@ -9,7 +9,6 @@ data("jtrain2")
 y <- "re78"    #outcome: 1 variable
 x <- c("re74", "re75", "age", "agesq", "nodegree")   # covariates
 w <- "train"   # treatment: 1 variable
-z <- c("age", "mostrn")   #selection: 2 variables
 
 JTRAIN2_W1 <- jtrain2 %>% filter(train==1)
 JTRAIN2_W0 <- jtrain2 %>% filter(train==0)
@@ -21,7 +20,6 @@ y1_x <-predict(lm1,type="response",jtrain2)
 
 lm0<-lm(fmla,data=JTRAIN2_W0)
 y0_x <-predict(lm0,type="response",jtrain2)
-jtrain2$my_cate<-y1_x - y0_x
 
 ATE<- mean (y1_x - y0_x)  #AVERAGE TREATEMENT EFFECT    .4035484
 POmean <- mean(y0_x)      #AVERAGE TREATEMENT EFFECT ON NON TREATED  4.598974
